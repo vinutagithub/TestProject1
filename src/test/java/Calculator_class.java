6369589044c6df7ea1d29a8e51f3140671e1e827
vinutagithub <vinuta_ktk@yahoo.co.in>
2020-10-28 15:26:26 -0700

    project created

---
+++ src/test/java/Calculator_class.java
@@ -0,0 +1,29 @@
+public class Calculator_class {
+
+    int a;
+    int b;
+
+    public Calculator_class(){}
+
+    public Calculator_class(int a, int b){
+
+        this.a = a;
+        this.b = b;
+
+    }
+
+    private float multiplication(int a, int b) {
+        new Calculator_class(a,b);
+        return (a*b);
+    }
+
+    private int subtraction(int a, int b) {
+        new Calculator_class(a,b);
+        return(a-b);
+    }
+
+    public int addition(int a, int b) {
+        new Calculator_class(a,b);
+        return(a+b);
+    }
+}
